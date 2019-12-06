9597a9652d301a97730b604edc41e9f34b4848c7
marcosxz <1214934780@qq.com>
2019-12-06 15:02:48 +0800

    支持多个客户端注册

--- client_test.go
+++ client_test.go
@@ -3,20 +3,15 @@ package etcdkv
 import "testing"
 
 func TestNewClientV3(t *testing.T) {
-	client, err := NewClientV3(ClientEndpoints("127.0.0.1:2379"))
-	if err != nil {
+	if err := NewClientV3(ClientEndpoints("127.0.0.1:2379")); err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
 
-	t.Log("NewClientV3:", client)
-
-	client, ok := ClientV3()
-	if !ok {
-		if err != nil {
-			t.Error("client not found")
-			t.FailNow()
-		}
+	client := ClientV3(DefaultClientName)
+	if client == nil {
+		t.Error("client not found")
+		t.FailNow()
 	}
 
 	t.Log("ClientV3:", client)
